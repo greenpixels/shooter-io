144ed9fdbe77dbcc42323a566b13a0b2b5ba7b3a
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-11-02 21:18:44 +0100

    chore: remove unused test listener

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,6 @@ import { io } from 'socket.io-client'
 
 const socket = io("ws://localhost:8080")
 
-socket.on("message", text => {
-  console.log(text)
-})
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App socket={socket} />
