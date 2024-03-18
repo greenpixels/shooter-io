f17ab8655aa72d389a0dcb662c64032851901aff
Sven | @greenpixels_ <58704291+greenpixels@users.noreply.github.com>
2024-03-18 21:55:31 +0100

    feat: add deployment to a production server (#7)
    * ci: rename workflows and add deployment file
    
    * ci: trigger deployment after lifecycle ends if branch is master
    
    * ci: attemps to fix workflow linting by using single quotes
    
    * ci: temp remove if statement to test trigger
    
    * ci: use pat to grant permissions to trigger workflow
    
    * ci: fix creds by adding additional curly braces
    
    * ci: add comment to remind for token invalidation
    
    * ci: configure build step for server
    
    * ci: genereate artifacts from building
    
    * ci: fix indent overuse
    
    * ci: configure deployment using scp
    
    * ci: use correct secret name for deployment
    
    * ci: store private key in environment
    
    * ci: fix missing dollar sign
    
    * ci: encapsulate variable in brackets
    
    * ci: disable strict host key check
    
    * ci: output folder structure for testing
    
    * ci: use run id to download artifacts from triggered workflow
    
    * ci: add PAT to artifact download
    
    * ci: use workflow run instead of workflow dispatch
    
    * ci: fix naming error
    
    * ci: transfer run id from lifecycle to deployment to access artifacts
    
    * ci: declare origin_run_id as input variable
    
    * feat: define folder paths for artifact download
    
    * ci: setup docker compose images and start them via ssh
    
    * ci: give docker network specific names to prevent conflict
    
    * ci: use custom networks in services
    
    * ci: fix error by listing networks as yaml array
    
    * ci: only deploy on main branch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { io } from 'socket.io-client'
 
-const socket = io("ws://localhost:8080")
+const target = import.meta.env.VITE_TARGET
+if(target === undefined) {
+  throw Error("The process could not start because the environment variable for TARGET was not set propertly (undefined)")
+}
+const socket = io("ws://" + target)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
