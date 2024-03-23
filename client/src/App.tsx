249eed279b7583e46097ba015c4c4c941264168a
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2024-03-23 23:06:05 +0100

    feat: add mouse event listener and pass in canvasSize to client handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import { useEffect, useRef, useState } from "react"
 import { Socket } from "socket.io-client"
 import * as PIXI from 'pixi.js'
 import { ClientGameHandler } from "./ClientGameHandler";
+import { Vector2DTO } from "@shared/dtos/Vector2DTO";
+import "./App.css"
 
-const canvasSize = { width: 640, height: 360 }
+const canvasSize: Vector2DTO = { x: 640, y: 360 }
 const canvasScale = 2
 
 function App(props: { socket: Socket }) {
@@ -20,32 +22,35 @@ function App(props: { socket: Socket }) {
       props.socket.disconnect()
     };
     if (game === null && viewRef) {
-      const newGame = new PIXI.Application<HTMLCanvasElement>({ view: viewRef, ...{canvasSize} });
+      const newGame = new PIXI.Application<HTMLCanvasElement>({ view: viewRef, width: canvasSize.x , height: canvasSize.y, backgroundColor:  0x808080})
       setGame(newGame)
-    } else if(game !== null && !running) {
+    } else if(game !== null && !running && viewRef) {
       setRunning(true)
-      const eventHandler = new ClientGameHandler({game: game, socket: props.socket})
+      const size = {x: canvasSize.x * canvasScale, y: canvasSize.y * canvasScale}
+      const eventHandler = new ClientGameHandler({game: game, socket: props.socket, canvasSize: size})
       
-      document.addEventListener("keydown", (ev) => eventHandler.handleInput(ev, true))
-      document.addEventListener("keyup", (ev) => eventHandler.handleInput(ev, false))
+      document.addEventListener("keydown", (ev) => eventHandler.handleKeyboardInput(ev, true))
+      document.addEventListener("keyup", (ev) => eventHandler.handleKeyboardInput(ev, false))
+      viewRef.addEventListener("mousemove", (ev) => eventHandler.handleMouseMoveInput(ev))
       window.addEventListener('beforeunload', handleTabClose);
       setEventHandler(eventHandler)
     }
 
     return () => {
-      if(game !== null && viewRef !== undefined && running) {
+      if(game && viewRef && running && eventHandler) {
         setGame(null)
         setRunning(false)
         window.removeEventListener('beforeunload', handleTabClose);
-        document.removeEventListener("keydown", (ev) => eventHandler?.handleInput(ev, true))
-        document.removeEventListener("keyup", (ev) => eventHandler?.handleInput(ev, false))
+        document.removeEventListener("keydown", (ev) => eventHandler.handleKeyboardInput(ev, true))
+        document.removeEventListener("keyup", (ev) => eventHandler.handleKeyboardInput(ev, false))
+        viewRef.addEventListener("mousemove", (ev) => eventHandler.handleMouseMoveInput(ev))
       }
     }
   }, [game, gameViewRef, running, eventHandler, props.socket]);
 
   return (
-    <div ref={gameParentRef}>
-      <canvas ref={gameViewRef} style={{ width: canvasSize.width*canvasScale, height: canvasSize.height*canvasScale }} width={canvasSize.width} height={canvasSize.height} />
+    <div ref={gameParentRef} className={"canvas-container"}>
+      <canvas ref={gameViewRef} style={{ width: canvasSize.x*canvasScale, height: canvasSize.y*canvasScale,  }} width={canvasSize.x} height={canvasSize.y} />
     </div>
   );
 }
