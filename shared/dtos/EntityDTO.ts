e30d328347c67b5483cfaf35f934dd8cec2fbec3
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2024-04-01 03:23:12 +0200

    build: ignore prettier in dtos as comma dangle is needed for ts-to-zod

--- shared/dtos/EntityDTO.ts
+++ shared/dtos/EntityDTO.ts
@@ -1,6 +1,11 @@
 import { Vector2DTO } from './Vector2DTO'
 
 export interface EntityDTO {
-    id: string
+    /**
+     * @format uuid
+     * @maxLength 50
+     * @minLength 2
+     */
+    id: string,
     position: Vector2DTO
 }
