86b8f25e7ae453b79c2482caa0ccc48e5ce0becc
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2024-04-01 22:38:22 +0200

    fix: use then instead of await

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Application, Renderer } from 'pixi.js'
-import '@pixi/gif'
 import { SetupHelper } from './classes/SetupHelper.ts'
+import '@pixi/gif'
+import 'pixi.js'
 
 const CANVAS_WIDTH = 640
 const CANVAS_HEIGHT = 380
 const socket = await SetupHelper.createSocketConnection()
 const application = new Application<Renderer<HTMLCanvasElement>>()
 
-await application.init({
-    width: CANVAS_WIDTH,
-    height: CANVAS_HEIGHT,
-    backgroundColor: 0x7f7f7f,
-})
-
-const clientEventHandler = SetupHelper.createClientHandlerWithEventListeners(socket, application)
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App
-            socket={socket}
-            application={application}
-            client={clientEventHandler}
-        />
-    </React.StrictMode>
-)
+application
+    .init({
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+        backgroundColor: 0x7f7f7f,
+    })
+    .then(() => {
+        const clientEventHandler = SetupHelper.createClientHandlerWithEventListeners(socket, application)
+        ReactDOM.createRoot(document.getElementById('root')!).render(
+            <App
+                socket={socket}
+                application={application}
+                client={clientEventHandler}
+            />
+        )
+    })
