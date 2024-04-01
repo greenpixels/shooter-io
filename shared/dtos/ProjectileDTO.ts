527d19a9441328891e8920eecfc91ca39f0cbda8
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2024-04-01 20:07:02 +0200

    feat: add player id as source to projectile dto

--- shared/dtos/ProjectileDTO.ts
+++ shared/dtos/ProjectileDTO.ts
@@ -2,5 +2,6 @@ import { EntityDTO } from './EntityDTO'
 import { Vector2DTO } from './Vector2DTO'
 
 export interface ProjectileDTO extends EntityDTO {
-    direction: Vector2DTO
+    direction: Vector2DTO,
+    sourcePlayerId: string
 }
