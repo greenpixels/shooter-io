f6b847f56e6754ee2c6f8190ddf53488b6634713
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2024-04-03 22:42:18 +0200

    refactor: remove class property and use local variable instead

--- client/src/classes/effects/ProjectileExplosionEffect.ts
+++ client/src/classes/effects/ProjectileExplosionEffect.ts
@@ -1,31 +1,29 @@
 import { Vector2DTO } from '@shared/dtos/Vector2DTO'
 import { Container } from 'pixi.js'
 import ExplosionImage from '@assets/spr_explosion.gif'
-import { AnimatedGIF } from '@pixi/gif'
 import { ImportHelper } from '../ImportHelper'
 
 export class ProjectileExplosionEffect {
     private static loadedGif = ImportHelper.loadGif(ExplosionImage)
-    localSprite: AnimatedGIF | undefined = undefined
 
     constructor(stage: Container, position: Vector2DTO) {
         this.instantiateInGame(stage, position)
     }
 
     async instantiateInGame(stage: Container, position: Vector2DTO) {
-        let loadedGif = await ProjectileExplosionEffect.loadedGif!
-        this.localSprite = loadedGif.clone()
-        this.localSprite.loop = false
-        this.localSprite.texture.source.scaleMode = 'nearest'
-        this.localSprite.anchor.set(0.5, 0.5)
-        this.localSprite.position.set(position.x, position.y)
-        this.localSprite.animationSpeed = 2
-        this.localSprite.angle = Math.random() * 360
-        this.localSprite.zIndex = position.y + 100
+        const loadedGif = await ProjectileExplosionEffect.loadedGif!
+        const sprite = loadedGif.clone()
+        sprite.loop = false
+        sprite.texture.source.scaleMode = 'nearest'
+        sprite.anchor.set(0.5, 0.5)
+        sprite.position.set(position.x, position.y)
+        sprite.animationSpeed = 2
+        sprite.angle = Math.random() * 360
+        sprite.zIndex = position.y + 100
 
-        this.localSprite.onComplete = () => {
-            stage.removeChild(this.localSprite as Container)
+        sprite.onComplete = () => {
+            stage.removeChild(sprite)
         }
-        stage.addChild(this.localSprite)
+        stage.addChild(sprite)
     }
 }
